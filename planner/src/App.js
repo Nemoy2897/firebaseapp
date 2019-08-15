c0ed233cc33b55a28e8a8d66834b864aa4c750fb
unknown <pranav.nema1997@gmail.com>
2019-08-16 00:49:29 +0530

    update components

--- planner/src/App.js
+++ planner/src/App.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import {BrowserRouter} from 'react-router-dom';
-// import logo from './logo.svg';
-// import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+import Dashboard from './components/dashboard/Dashboard'
+import ProjectDetails from './components/projects/ProjectDetails'
+import SignIn from './components/auth/SignIn'
+import SignUp from './components/auth/SignUp'
+import CreateProject from './components/projects/CreateProject'
 
-function App() {
-  return (
-    <BrowserRouter>
-     <div className="App">
-      <h1>the Planner</h1>
-    </div>
-    </BrowserRouter>
-   
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path='/'component={Dashboard} />
+            <Route path='/project/:id' component={ProjectDetails} />
+            <Route path='/signin' component={SignIn} />
+            <Route path='/signup' component={SignUp} />
+            <Route path='/create' component={CreateProject} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
