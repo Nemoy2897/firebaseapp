30951b6b575221e685b62489d15d7754f43d15d6
unknown <pranav.nema1997@gmail.com>
2019-08-16 03:42:29 +0530

    firebase wth redux connected

--- planner/src/store/actions/projectActions.js
+++ planner/src/store/actions/projectActions.js
@@ -1,6 +1,15 @@
 export const createProject = (project) => {
-return (dispatch, getState) => {
+return (dispatch, getState, {getFirebase,getFirestore} ) => {
     // mke async call to database
-    dispatch({type:'CREATE_PROJECT', project:project});
+    const firestore = getFirestore();
+    firestore.collection('projects').add(
+            {...project,
+            authorFirstName:'great',
+            authorLastName:'dane',
+            createdAt:new Date()
+        }
+    ).then(() => {dispatch({type:'CREATE_PROJECT', project:project})})
+    .catch((err) => dispatch({type:'CREATE_PROJECT_ERROR', err}) )
+    ;
 }
 };
